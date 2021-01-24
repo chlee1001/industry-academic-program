f891af26443d9d6ecb4fb55574eef50a72a80c90
Lee ChaeHyeon <chlee1001@naver.com>
2021-01-25 01:21:08 +0900

    21.01.25_EKAPEPIA_2차작업

--- EKAPEPIA_축산물_가격정보/README.md
+++ EKAPEPIA_축산물_가격정보/README.md
@@ -6,9 +6,16 @@
 
     기본 크롤링 셋팅완료
     DB 스키마 간단히 구성완료
+<img src="https://user-images.githubusercontent.com/2025813/105574974-6d54d900-5dab-11eb-82f6-bad929ed6ea6.jpg" width="50%" height="50%">
+
+
+#### 2차 작업
+    1일 치 테이블 읽기
+    --> 문제가 많음. colspan, rowspan... 어떻게 해야할지...
+        ++ 생각해봐야함... 하나하나 확인하면서 저장한 결과... test_result.json에서 볼 수 있음
+        
 
 #### 추후 진행할 것들
 
     1. 테이블에서 필요한 정보만 긁는 중
 
-![SmartSelect_20210121-152514_Samsung Notes](https://user-images.githubusercontent.com/2025813/105574974-6d54d900-5dab-11eb-82f6-bad929ed6ea6.jpg)
--- EKAPEPIA_축산물_가격정보/test2.py
+++ EKAPEPIA_축산물_가격정보/test2.py
@@ -4,7 +4,7 @@ from datetime import date, timedelta, datetime  # 최근 1년 날짜 구하기
 from pymongo import MongoClient
 
 # mongodb 연결객체 생성
-client = MongoClient('mongodb://chlee1001:dlcogus@192.168.1.2:27017')
+client = MongoClient('주소')
 db = client.EKAPEPIA
 
 searchStartDate = '2020-01-01'
@@ -39,8 +39,9 @@ menuIDs = ['menu100033', 'menu100034', 'menu100039', 'menu100155', 'menu100151']
 
 
 def getData(id, kind, menuID):
-    REQ_URL = ROOT_URL + kind + 'menuID=' + menuID + '&searchStartDate=' + searchEndDate + '&searchEndDate=' + searchEndDate
-
+    print(searchEndDate)
+    REQ_URL = ROOT_URL + kind + 'menuID=' + menuID + '&searchStartDate=' + '2021-01-22' + '&searchEndDate=' + '2021-01-22'
+    now_date = ''
     response = requests.get(REQ_URL)
     if response.status_code == 200:
         text = response.text
@@ -49,10 +50,86 @@ def getData(id, kind, menuID):
         # print(table)
         if table is not None:
             trs = table.find_all('tr')
-            for i in range(len(trs) - 1):
-                ths = trs[0].find_all('th')
-                for j in range(len(ths)):
-                    print(ths[j + 1].text)
+            # tr0~2까지 정보...
+
+            table_list = []
+            price_list = []
+            division1 = []
+            division2 = []
+            division3 = []
+            for idx, tr in enumerate(trs):
+                if idx == 0:
+                    ths = tr.find_all('th')
+                    for i in range(len(ths) - 1):  # (0~2)
+                        table_list.append(ths[i + 1].text)
+                        division1.append(ths[i + 1].text)
+
+
+                elif idx == 1:
+                    ths = tr.find_all('th')
+                    for i in range(len(ths)):  # (0~2)
+                        table_list.append(ths[i].text)
+                        division2.append(ths[i].text)
+                elif idx == 2:
+                    ths = tr.find_all('th')
+                    for i in range(len(ths)):  # (0~2)
+                        table_list.append(ths[i].text)
+                        division3.append(ths[i].text)
+                else:
+                    # print(tr)
+                    now_date = tr.find('th').text.replace(" ", "")
+                    # print(now_date)
+                    spans = tr.find_all('span')
+                    for span in spans:
+                        if len((span.text.split())) < 1:  # table에 가격 셀이 비어있는 경우 '-' 출력
+                            price_list.append("-")
+                        else:
+                            price_list.append(span.text.split()[0])
+            # print(table_list)
+            # print(price_list)
+
+            # for i in range(len(price_list)):
+            #     test1[table_list[len(table_list) - len(price_list) + i]] = price_list[i]
+            #     # print(table_list[len(table_list) - len(price_list) + i])
+            #     # print(table_list[len(table_list) - len(price_list):])
+            # print(test1)
+            # for j in range(4):
+            #     print(table_list[len(table_list) - len(price_list) - 4 + j])
+
+            row_division1 = {}  # 경매가격
+            row_division2 = {}  # 지육가격
+            row_division3 = {}  # 부분육가격
+            row_division4 = {}  # 한우등심
+
+            # 개수는 colspan과 rowspan으로 일단 파악해보기.. 안되면 어쩔수 없고
+            for i in range(3):
+                row_division1[division3.pop(0)] = price_list.pop(0)
+            for i in range(2):
+                row_division2[division3.pop(0)] = price_list.pop(0)
+            row_division3[division3.pop(0)] = price_list.pop(0)
+            row_division4[division3.pop(0)] = price_list.pop(0)
+
+            test1 = dict()
+            test2 = dict()
+            test3 = dict()
+            test4 = dict()
+            test1[division2[0]] = row_division1
+            test2[division2[1]] = row_division2
+            test3[division2[2]] = row_division3
+            test4[division2[3]] = row_division4
+
+
+            dic1 = {}  # 산지가격
+            dic2 = {}  # 도매가격
+            dic3 = {}  # 소비자가격
+            dic1[division1[0]] = test1
+            dic2[division1[1]] = test2, test3
+            dic3[division1[2]] = test4
+
+
+            result={}
+            result[now_date] = dic1, dic2,dic3
+            print(result)
 
 
 def saveDB():
